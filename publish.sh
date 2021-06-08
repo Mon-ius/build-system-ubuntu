ab02fe5c981230a9728999da4c0cec8677b4f0a3
Monius <m1105930979@gmail.com>
2021-06-09 07:20:36 +0800

    add run

--- publish.sh
+++ publish.sh
@@ -3,4 +3,5 @@ docker build -t build-system-ubuntu:base .
 docker tag build-system-ubuntu:base monius/build-system-ubuntu:base
 docker push build-system-ubuntu:base
 
+#docker pull monius/build-system-ubuntu:base
 #docker run -it --rm --cap-add=NET_ADMIN --cap-add=SYS_ADMIN --device=/dev/net/tun monius/build-system-ubuntu:base /bin/bash
